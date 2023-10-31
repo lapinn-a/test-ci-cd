46d5eb1eedf599192f77fd023d653c9f9945bbfc
lapinn-a <lapinn-a@ya.ru>
2023-10-31 19:55:18 +0300

    add 1

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func hello(w http.ResponseWriter, req *http.Request) {
-	fmt.Fprintf(w, "hello\n")
+	fmt.Fprintf(w, "hello 1\n")
 }
 
 func headers(w http.ResponseWriter, req *http.Request) {
